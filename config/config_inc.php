bced7584ed71d9cdaf881d89380f5dcbc526d282
Blessan Thomas <195085@ust-global.com>
2022-01-14 18:49:58 +0530

    First commit

---
+++ config/config_inc.php
@@ -0,0 +1,22 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'XUZVcskgBX+SaC/GXKzbniSxwsmKrrT8RdeEy1EfX30=';
+
+$g_allow_signup  = ON;  
+$g_enable_email_notification = ON; 
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'smtp.gmail.com';
+$g_smtp_connection_mode = 'tls';
+$g_smtp_port = 587;
+$g_smtp_username = 'ustqet@gmail.com'; //your email address
+$g_smtp_password = 'Ust@1234'; //your email address password
+$g_administrator_email = 'ustqet@gmail.com'; //your email address
+$g_log_level = LOG_EMAIL | LOG_EMAIL_RECIPIENT | LOG_FILTERING | LOG_AJAX;
+$g_log_destination = "C:\xampp\mailoutput";
